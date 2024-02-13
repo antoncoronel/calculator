2ec87e169fc418d6582d7d3879006362d35f9b36
antoncoronel <antoncoronel@gmail.com>
2024-02-13 20:49:35 +0800

    Changed box div
    Moved box div from app.js to index.html to facilitate adding number and operator labels.

--- app.js
+++ app.js
@@ -27,17 +27,6 @@ function operate(a, b, op){
     }
 };
 
-const wrapper = document.querySelector('div.wrapper');
-
-for(let i = 0; i < 16; i++){
-    const box = document.createElement("div");
-    box.id = "box";
-    box.style.flex = `1 0 calc(100% / 5)`; // number of boxes per row
-    box.style.width = `calc(100% / 5)`;//set width of box
-    box.style.height = box.style.width;
-    box.style.border = "1px solid black"; // Set border style
-    wrapper.appendChild(box);
-}
 
 console.log(add(5, 4));
 console.log(subtract(5, 4));
