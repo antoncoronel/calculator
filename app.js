2cb4424629bdfc967c6b33f8c83801d056614467
antoncoronel <antoncoronel@gmail.com>
2024-02-13 20:04:34 +0800

    Added base files

---
+++ app.js
@@ -0,0 +1,33 @@
+function add(a, b){
+    return a + b;
+};
+
+function subtract(a, b){
+    return a - b;
+};
+
+function multiply(a, b){
+    return a*b;
+};
+
+function divide(a, b){
+    return a/b;
+};
+
+function operate(a, b, op){
+    switch(op){
+        case '+':
+            return add(a,b);
+        case '-':
+            return subtract(a,b);
+        case '*':
+            return multiply(a,b);
+        case '/':
+            return divide(a,b);
+    }
+};
+
+console.log(add(5, 4));
+console.log(subtract(5, 4));
+console.log(multiply(5,4));
+console.log(divide(5,4));
