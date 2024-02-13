f936cad1f381bbbdae5da24ff1b57f32a795f0fa
antoncoronel <antoncoronel@gmail.com>
2024-02-13 20:33:10 +0800

    Added basic operation functions

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function divide(a, b){
 };
 
 function operate(a, b, op){
-    switch(op){
+    switch(op){ //selects which operator to use
         case '+':
             return add(a,b);
         case '-':
@@ -26,6 +26,18 @@ function operate(a, b, op){
             return divide(a,b);
     }
 };
+
+const wrapper = document.querySelector('div.wrapper');
+
+for(let i = 0; i < 16; i++){
+    const box = document.createElement("div");
+    box.id = "box";
+    box.style.flex = `1 0 calc(100% / 5)`; // number of boxes per row
+    box.style.width = `calc(100% / 5)`;//set width of box
+    box.style.height = box.style.width;
+    box.style.border = "1px solid black"; // Set border style
+    wrapper.appendChild(box);
+}
 
 console.log(add(5, 4));
 console.log(subtract(5, 4));
