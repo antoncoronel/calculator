19d7b2c4d7352a2fafb26c456e40e7cd41a9425d
antoncoronel <antoncoronel@gmail.com>
2024-02-28 21:38:58 +0800

    Updated operator algorithm

--- app.js
+++ app.js
@@ -28,22 +28,28 @@ function operate(a, b, op){
 };
 
 function displayCalcNum(){
-    const screen = document.querySelector(".current");
-    screen.textContent = appendStr;
+    const current = document.querySelector(".current");
+    current.textContent = appendStr;
 }
 
 function displayCalcClear(){
-    const screen = document.querySelector(".current");
-    screen.textContent = "";
+    const current = document.querySelector(".current");
+    const previous = document.querySelector(".previous");
+    const operator = document.querySelector(".op");
+    current.innerHTML = "";
+    previous.innerHTML = "";
+    operator.innerHTML = "";
+    console.log("run");
 }
 
 let appendStr = "";
 
 const numButton = document.querySelectorAll("#num");
 const operatorButton = document.querySelectorAll("#operate");
+const equalsButton = document.querySelector("#equals")
 const previousDisplay = document.querySelector('.previous');
 const currentDisplay = document.querySelector('.current');
-
+const operatorDisplay = document.querySelector(".op");
 
 numButton.forEach(button => {
     button.addEventListener('click', (e) => {
@@ -51,22 +57,31 @@ numButton.forEach(button => {
         appendStr += value;
         displayCalcNum();
     })
-})
+});
 
 operatorButton.forEach(button => {
     button.addEventListener('click', (e) => {
         const operator = e.target.textContent;
         console.log('operator clicked');
-        if(previousDisplay.innerHTML == ""){
+        if(previousDisplay.innerHTML == ""){ //before operator has been pressed
             previousDisplay.innerHTML = appendStr;
+            operatorDisplay.innerHTML = operator;
             appendStr = "";
+            currentDisplay.innerHTML = "";
         }
-        else if (previousDisplay.innerHTML != ""){
-            const ans = operate(previousDisplay.innerHTML, currentDisplay.innerHTML, operator);
-            previousDisplay.innerHTML = "";
+        else if (previousDisplay.innerHTML != "" && currentDisplay.innerHTML != ""){ //operator has been pressed with a second number
+            const ans = operate(parseFloat(previousDisplay.innerHTML), parseFloat(currentDisplay.innerHTML), operator);
             currentDisplay.innerHTML = ans;
         }
+
     })
+});
+
+equalsButton.addEventListener('click', () => {
+    console.log("equals");
+    if (previousDisplay.innerHTML != "" && currentDisplay.innerHTML != ""){
+        const ans = operate(parseFloat(previousDisplay.innerHTML), parseFloat(currentDisplay.innerHTML), operatorDisplay.innerHTML);
+        displayCalcClear();
+        currentDisplay.innerHTML = ans;
+    }
 })
-
-
